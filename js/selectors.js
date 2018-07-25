ad73d32203b2029c8f076d129787e71eeec99fdc
Linda Chang <lindachang186@gmail.com>
2018-07-25 21:23:45 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,13 @@
-// declare your functions here...
+// declare your functions here.
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  $("img[id ='baby-ninja']")
+}
